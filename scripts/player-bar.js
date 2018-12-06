3b91a2cfdf28a7fb376f2fec44bcc346f498c4a9
Josasia <Josasia@outlook.com>
2018-12-06 11:48:08 -0800

    fixed errors, assignment complete

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -6,7 +6,6 @@
 
   $('button#next').on('click', function() {
     if (player.playState !== 'playing') { return; }
-
     const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
     const nextSongIndex = currentSongIndex + 1;
     if (nextSongIndex >= album.songs.length) { return; }
@@ -14,14 +13,13 @@
     player.playPause(nextSong);
   });
 
-
   $('button#previous').on('click', function() {
     if (player.playState !== 'playing') { return; }
+    const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
     const previousSongIndex = currentSongIndex - 1;
-    if (previousSongIndex >= '0') { return; }
+    if (previousSongIndex >= album.songs.length) { return; }
 
-    const previousSong = album.songs[nextSongIndex];
+    const previousSong = album.songs[previousSongIndex];
     player.playPause(previousSong);
-
-
 });
+}

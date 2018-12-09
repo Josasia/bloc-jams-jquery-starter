0cfe78f331bbd2ebdb290a20bb033f0c74a6e9b4
Josasia <Josasia@outlook.com>
2018-12-09 15:23:44 -0800

    fixing time method

--- scripts/helper.js
+++ scripts/helper.js
@@ -3,8 +3,11 @@
 class Helper {
   playPauseAndUpdate(song) {
     player.playPause(song);
-    const player.currentlyPlaying.duration;
-
+    const player = {
+      currentlyPlaying: {
+        duration: 0
+      }
+    };
   }
 };
   const helper = new Helper();

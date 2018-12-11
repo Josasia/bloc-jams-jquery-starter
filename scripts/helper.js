0f801aaa94c5870ac9c8e884f633abdf6aa29983
Josasia <Josasia@outlook.com>
2018-12-11 09:20:02 -0800

    prettyTime almost there

--- scripts/helper.js
+++ scripts/helper.js
@@ -1,18 +1,17 @@
 
 
 class Helper {
-  
+
   prettyTime (timeInSeconds) {
     const min = Math.floor( timeInSeconds / 60);
     const sec = Math.floor( timeInSeconds - min * 60);
-    return $(min}:${sec};
+   return `${min}:${sec}`;
   }
 
   playPauseAndUpdate(song) {
     player.playPause(song);
     const totalTime = player.currentlyPlaying.duration;
-    const prettyTotalTime = player.prettyTime(totalTime);
-    $('#time-control .total-time').text(totalTime);
+    $('#time-control .total-time').text(this.prettyTime(totalTime));
   }
 };
   const helper = new Helper();
